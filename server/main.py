b775a95767f1a4f9719c2c14a047c5ee5f3ea9d7
Jasmine <jasmineqw45@gmail.com>
2024-03-23 15:43:58 -0500

    added main.py python server with post and get request

---
+++ server/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Name(BaseModel):
+    firstName: str = "Jas"
+    lastName: str = "Wu"
+
+app = FastAPI(
+    
+)
+
+@app.get("/helloworld")
+# function handles requests that go to path "/" with the "get" operation
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.post("/name")
+async def create_name(name:Name):
+    # f embeds code into a string
+    return {f"Hello, {name.firstName} {name.lastName}"}
