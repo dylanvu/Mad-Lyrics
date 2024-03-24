e79391189fd0f21d42d228736e46b272fd495aa4
Jasmine <jasmineqw45@gmail.com>
2024-03-24 06:47:59 -0500

    feat: plz god

--- client/src/app/lobby/page.tsx
+++ client/src/app/lobby/page.tsx
@@ -1,32 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext } from "react";
 
 import { WebsocketContext } from "@/components/socket";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Dices, Loader, User } from "lucide-react";
-
-function uuidToNumber(uuid: string) {
-    let hash = 0;
-    for (let i = 0; i < uuid.length; i++) {
-        hash += uuid.charCodeAt(i);
-    }
-    return hash;
-}
+import { Dices, User } from "lucide-react";
 
 export default function Lobby() {
     const ws = useContext(WebsocketContext);
     const router = useRouter();
-
-    const [players, setPlayers] = useState<string[]>([
-        "fsdfsdf",
-        "joe",
-        "supergirlygmaer",
-    ]);
-
-    const [loading, setLoading] = useState(false);
 
     const handleStart = () => {
         // send start event to all clients
@@ -37,80 +20,130 @@ export default function Lobby() {
     };
 
     useEffect(() => {
-        setPlayers(players);
-
-        console.log(players);
-    }, [players, ws.players]);
-
-    useEffect(() => {
         if (ws.phase === "input") {
             router.push("/"); // FIXME: When we swap URLs, make sure to update this too
-        } else if (ws.phase === "lobby_wait") {
-            setLoading(true);
         }
     }, [router, ws.phase]);
 
     return (
         <main className="flex-between h-screen min-h-screen bg-[url('/images/lobbyBackground.svg')] p-16">
-            {loading ? (
-                <div className="flex-center my-auto flex-col space-y-4 text-white">
-                    <Loader className="h-24 w-24 animate-spin transition duration-3000" />
-                    <span className="text-3xl font-semibold">
-                        Generating your{" "}
-                        <span className="font-bold text-jas-purple">
-                            Mad Lyrics!
-                        </span>{" "}
-                        Hang tight 😼
-                    </span>
-                </div>
-            ) : (
-                <>
-                    <div className="flex-between h-full min-w-[500px] flex-col rounded-3xl bg-[#191B21] px-9 pb-9 pt-12 text-white">
-                        <div className="w-full">
-                            <div className="flex-between pb-8">
-                                <h2
-                                    className="h-full text-4xl font-black text-jas-purple"
-                                    style={{
-                                        WebkitTextStroke: "white",
-                                        WebkitTextStrokeWidth: 2,
-                                    }}
-                                >
-                                    Lobby
-                                </h2>
-                                <p className="text-2xl font-bold">3 players</p>
-                            </div>
+            {/* <div className="main-div">lobby</div>
+            <Button onClick={handleStart}>Start Game</Button>
+            <p>{ws.ready ? "Connected!" : "Not connected )="}</p> */}
 
-                            <div className="mb-auto space-y-4">
-                                {players.map((player) => (
-                                    <div
-                                        className="flex-between rounded-2xl border-4 border-jas-gray bg-jas-card p-4 py-1 text-white hover:border-jas-purple"
-                                        key={player}
-                                    >
-                                        <img
-                                            src={
-                                                uuidToNumber(player) % 3 == 0
-                                                    ? "./images/cat.svg"
-                                                    : uuidToNumber(player) %
-                                                            3 ==
-                                                        1
-                                                      ? "./images/bird.svg"
-                                                      : "./images/mouse.svg"
-                                            }
-                                            alt="cat"
-                                            className="scale-90"
-                                        />
-                                        <p className="text-3xl font-bold">
-                                            {player}
-                                        </p>
-                                    </div>
-                                ))}
+            <div className="flex-between h-full min-w-[500px] flex-col rounded-3xl bg-[#191B21] px-9 pb-9 pt-12 text-white">
+                {/* lobby tag */}
+                <div className="w-full">
+                    <div className="flex-between pb-8">
+                        <h2
+                            className="h-full text-4xl font-black text-jas-purple"
+                            style={{
+                                WebkitTextStroke: "white",
+                                WebkitTextStrokeWidth: 2,
+                            }}
+                        >
+                            Lobby
+                        </h2>
+                        <p className="text-2xl font-bold">3 players</p>
+                    </div>
 
-                                <p className="pt-2 text-center text-xl font-bold text-white text-opacity-75">
-                                    waiting for more players...
-                                </p>
-                            </div>
+                    <div className="mb-auto space-y-4">
+                        <div className="flex-between rounded-2xl border-4 border-jas-gray bg-jas-card p-4 py-1 text-white hover:cursor-pointer hover:border-jas-purple">
+                            <img
+                                src="./images/cat.svg"
+                                alt="cat"
+                                className="scale-100"
+                            />
+                            <p className="text-3xl font-bold">SuperEpicGamer</p>
+                        </div>
+                        <div className="flex-between rounded-2xl border-4 border-jas-gray bg-jas-card p-4 py-1 text-white hover:cursor-pointer hover:border-jas-purple">
+                            <img
+                                src="./images/bird.svg"
+                                alt="cat"
+                                className="scale-100"
+                            />
+                            <p className="text-3xl font-bold">SuperEpicGamer</p>
+                        </div>
+                        <div className="flex-between rounded-2xl border-4 border-jas-gray bg-jas-card p-4 py-1 text-white hover:cursor-pointer hover:border-jas-purple">
+                            <img
+                                src="./images/mouse.svg"
+                                alt="cat"
+                                className="scale-90"
+                            />
+                            <p className="text-3xl font-bold">SuperEpicGamer</p>
                         </div>
 
+                        <p className="pt-2 text-center text-xl font-bold text-white text-opacity-75">
+                            waiting for more players...
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="profile-contain">
+                <div className="heading">
+                    <h2>Customize profile</h2>
+                    <div className="flex-center hover:bg-[# h-[48px] w-[48px] rounded-2xl bg-jas-purple hover:cursor-pointer">
+                        <Dices
+                            className="h-[30px] w-[30px] p-[2px]"
+                            stroke="white"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex-center mb-auto w-full flex-col">
+                <h1
+                    className="text-center text-6xl font-black text-jas-purple"
+                    style={{
+                        WebkitTextStroke: "white",
+                        WebkitTextStrokeWidth: 3,
+                        marginBottom: "28px",
+                    }}
+                >
+                    MadLyrics
+                </h1>
+
+                <div className="flex-center flex-col space-y-2">
+                    <div className="filters-contain">
+                        <div className="heading">
+                            <h2>Set music filters</h2>
+                            <div className="flex-center hover:bg-[# h-[48px] w-[48px] rounded-2xl bg-jas-purple hover:cursor-pointer">
+                                <Dices
+                                    className="h-[30px] w-[30px] p-[2px]"
+                                    stroke="white"
+                                />
+                            </div>
+                        </div>
+                        {/* genre and emotion */}
+                        <div className="row">
+                            <form action="/submit-genre" method="post">
+                                <label htmlFor="music-genre">Genre</label>{" "}
+                                <input
+                                    type="text"
+                                    id="music-genre"
+                                    name="music-genre"
+                                    placeholder="edm"
+                                />
+                            </form>
+                            <form action="/submit-emotion" method="post">
+                                <label htmlFor="music-genre">Emotion</label>{" "}
+                                <input
+                                    type="text"
+                                    id="music-emotion"
+                                    name="music-emotion"
+                                    placeholder="exciting"
+                                />
+                            </form>
+                        </div>
+                        <form action="/submit-topic" method="post">
+                            <label htmlFor="music-topic">Topic</label>{" "}
+                            <input
+                                type="text"
+                                id="music-topic"
+                                name="music-topic"
+                                placeholder="funny bunnies"
+                            />
+                        </form>
                         <Button
                             className="h-20 w-full rounded-2xl bg-jas-purple px-8 py-6 hover:bg-jas-purple/80"
                             onClick={handleStart}
@@ -121,52 +154,26 @@ export default function Lobby() {
                         </Button>
                     </div>
 
-                    <div className="flex-center mb-auto w-full flex-col">
-                        <h1
-                            className="text-center text-8xl font-black text-jas-purple"
-                            style={{
-                                WebkitTextStroke: "white",
-                                WebkitTextStrokeWidth: 5,
-                            }}
-                        >
-                            MadLyrics
-                        </h1>
+                    {/* <img
+                        src="./images/big_cat.svg"
+                        alt="big cat"
+                        className="scale-75"
+                    /> */}
 
-                        <div className="flex-center flex-col space-y-2">
-                            <h3 className="mt-12 text-5xl font-bold text-white">
-                                Randomize your profile
-                            </h3>
-
-                            <img
-                                src="./images/big_cat.svg"
-                                alt="big cat"
-                                className="scale-75"
-                            />
-
-                            <div className="flex space-x-4">
-                                <div className="flex-between space-x-2 rounded-2xl border-4 border-jas-gray bg-jas-card p-4 text-white hover:border-jas-purple">
-                                    <div className="flex-center h-12 w-12 rounded-2xl bg-jas-gray">
-                                        <User
-                                            fill="white"
-                                            className="h-8 w-8"
-                                        />
-                                    </div>
-                                    <p className="text-3xl font-bold">
-                                        SuperCoolGamer
-                                    </p>
-                                </div>
-
-                                <div className="flex-center h-[88px] w-[88px] rounded-2xl bg-jas-purple">
-                                    <Dices
-                                        className="h-14 w-14"
-                                        stroke="white"
-                                    />
-                                </div>
+                    <div className="flex space-x-4">
+                        {/* <div className="flex-between space-x-2 rounded-2xl border-4 border-jas-gray bg-jas-card p-4 text-white hover:border-jas-purple">
+                            <div className="flex-center h-12 w-12 rounded-2xl bg-jas-gray">
+                                <User fill="white" className="h-8 w-8" />
                             </div>
+                            <p className="text-3xl font-bold">SuperCoolGamer</p>
                         </div>
+
+                        <div className="flex-center h-[88px] w-[88px] rounded-2xl bg-jas-purple">
+                            <Dices className="h-14 w-14" stroke="white" />
+                        </div> */}
                     </div>
-                </>
-            )}
+                </div>
+            </div>
         </main>
     );
 }
